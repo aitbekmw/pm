a58b72544ac0ec84c243e7714027bcbe7617f2eb
Мухаммад Аскеров <m.askerov@mdigital.kg>
2025-10-01 16:06:29 +0600

    'delete'

--- src/core/routers.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from src.user.router import router as user_router
-from src.project.router import router as project_router
-from src.meeting.router import router as meeting_router
-
-api_router = APIRouter()
-api_router.include_router(user_router)
-api_router.include_router(project_router)
-api_router.include_router(meeting_router)
