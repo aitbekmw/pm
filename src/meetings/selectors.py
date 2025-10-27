589199b1200306737256ed0ecafafbfb3882c839
Мухаммад Аскеров <m.askerov@mdigital.kg>
2025-10-27 16:00:54 +0600

    bug fix

--- src/meetings/selectors.py
+++ src/meetings/selectors.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy import select, and_, or_, func, joinedload
+from sqlalchemy import select, and_, or_, func
+from sqlalchemy.orm import joinedload
 from typing import Optional
 from datetime import datetime
 
