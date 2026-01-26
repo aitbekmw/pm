d042469dabbce3c509bad9f44d33bbbcd3c54c1f
Muhammad <m.askerov@mdigital.kg>
2026-01-26 12:18:24 +0600

    add cors meet.google.com

--- src/main.py
+++ src/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"] if settings.debug else [],
+    allow_origins=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173", "https://meet.google.com"] if settings.debug else [],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"],
