affdf3f02e4d0ed63f3169ae735612e54d2bb619
Muhammad <m.askerov@mdigital.kg>
2026-02-18 18:50:14 +0600

    fix migration 005

--- alembic/versions/005_add_company_entity.py
+++ alembic/versions/005_add_company_entity.py
@@ -13,7 +13,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = '005_add_company_entity'
-down_revision: Union[str, None] = '004_add_pdf_file_path_to_meetings'
+down_revision: Union[str, None] = '004'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
