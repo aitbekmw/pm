f2bb98e442e1041f80efc5eb381963115eec86d3
Muhammad <m.askerov@mdigital.kg>
2026-02-19 08:10:43 +0600

    fix migration 006 007

--- alembic/versions/007_add_importance.py
+++ alembic/versions/007_add_importance.py
@@ -1,7 +1,7 @@
 """add importance to meetings
 
-Revision ID: 007_add_importance_to_meetings
-Revises: 006_add_project_id_to_notifications
+Revision ID: 007_add_importance
+Revises: 006_add_project_id
 Create Date: 2026-02-12 16:15:00.000000
 
 """
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '007_add_importance_to_meetings'
-down_revision: Union[str, None] = '006_add_project_id_to_notifications'
+revision: str = '007_add_importance'
+down_revision: Union[str, None] = '006_add_project_id'
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
